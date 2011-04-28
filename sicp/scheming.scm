d941674874c158d4bab29eecd602dba533b9691d
Austin Soplata <austin.soplata@gmail.com>
2011-04-27 21:02:28 -0400

    deleted:    hack.cpp 	deleted:    hackout 	deleted:    old_c/firstprogz 	deleted:    old_c/firstprogz.c 	deleted:    sicp/scheming 	modified:   sicp/scheming.scm 	c/ 	cpp/ 	sicp/.sicpnotes.scm.swp 	sicp/Link to Structure and Interpretation of Computer Programs.pdf 	sicp/sicpnotes.scm 	sicp/sicpnotes.scm~

--- sicp/scheming.scm
+++ sicp/scheming.scm
@@ -1,1 +1,27 @@
-(* 7 7 7)
+(define (a-plus-abs-b a b) 
+  ( (if (> b 0)
+	+
+	-)
+    a b)) ; zomfg, operators as operands decided from the if, then, due to 
+          ; define placement, acting as operators on a and b
+i
+(define a 7)
+(define b 5)
+(define c 6)
+
+(if (or (> a b) (> a c)) 
+    (* a a)
+    "not a")
+(if (or (> b a) (> b c))  
+    (* b b) 
+    "not b")
+(if (or (> c a) (> c b))
+    (* c c)
+    "not c")
+
+
+
+
+
+
+
